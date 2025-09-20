b086a27e33fb1f11d0f0accafaa017d84d6a9cc4
mohamed-bakheet <midoyugioh@gmail.com>
2025-09-20 20:58:16 +0300

    update errors

--- src/app/action/categories.action.ts
+++ src/app/action/categories.action.ts
@@ -1,25 +1,28 @@
 "use server"
 
 import axios from "axios"
+import { Categories } from '../types/category.model';
 
-async function getCategories() {
+async function getCategories(): Promise<{ data: Categories[]; status: number; message: string }> {
     try {
-      const response = await  axios.get("https://ecommerce.routemisr.com/api/v1/categories")
-return{
-    data: response?.data.data ?? [],
-    status: response.status,
-    message: response?.data.message
+        const response = await axios.get("https://ecommerce.routemisr.com/api/v1/categories");
+        return {
+            data: response?.data.data ?? [],
+            status: response.status,
+            message: response?.data.message,
+        };
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            return {
+                data: [],
+                status: error.response?.status || 500,
+                message: error?.response?.data.message || "An error occurred",
+            };
+        }
+        throw error;
+    }
 }
-    } catch (error :unknown) {
-        if (axios.isAxiosError(error)) 
-     return{  
-         data: [],
-    status: error.response?.status,
-    message: error?.response?.data.message || "An error occurred"
-     }
-    
-}
-}
+
 
 async function getOneCategories(id:string) {
     try {
