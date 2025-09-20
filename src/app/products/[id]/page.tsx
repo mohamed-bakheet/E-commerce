278b1e44416ef407fb705d7b746ad01f46e813bd
mohamed-bakheet <midoyugioh@gmail.com>
2025-09-20 21:19:47 +0300

    update errors

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,7 +4,13 @@ import React from 'react'
 
 export default async function ProductDetails({params}:{params:{id:string}}) {
     const{id} = await params;
-    const {data: ProductDetails} = await getProductDetails(id);
+    const response = await getProductDetails(id);
+
+if (!response || !response.data) {
+  return <div className="text-center my-10">Error: Unable to fetch product details.</div>;
+}
+
+const { data: ProductDetails } = response;
    
   return (
     <div className='container text-center mx-auto my-10 max-w-9/10 h-max'>
