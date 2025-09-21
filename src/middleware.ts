9457c4c51ccaf6d84bab780e38345a1e364cbcf6
mohamed-bakheet <midoyugioh@gmail.com>
2025-09-21 22:05:50 +0300

    fix rsponse error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,21 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-const token = request.cookies.get('next-auth.session-token') || request.cookies.get("_vercel_jwt")?.value || request.cookies.get("__Secure-next-auth.session-token")?.value;
-if(!token){
-    return NextResponse.redirect(new URL('/login', request.url))
-}
+const token =
+  request.cookies.get("next-auth.session-token")?.value ||
+  request.cookies.get("__Secure-next-auth.session-token")?.value ||
+  request.cookies.get("_vercel_jwt")?.value;
+  const { pathname } = request.nextUrl;
 
+  // Protect private routes
+  if (!token && ["/products", "/cart", "/checkout", "/category", "/", "/wishlist"].includes(pathname)) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // Prevent logged-in users from accessing login page
+  if (token && pathname === "/login") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 
   return NextResponse.next()
 }
